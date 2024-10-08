3a9cbd9377d9940ee86cd06305f5eb1a888acb3e
Anilton Vigânigo Júnior <avjrawscloudstudies@gmail.com>
2024-10-07 23:01:39 -0300

    Solution without Default Methods

---
+++ src/model/services/BrazilInterestService.java
@@ -0,0 +1,28 @@
+package model.services;
+
+import java.security.InvalidParameterException;
+
+public class BrazilInterestService implements InterestService {
+	
+	private Double interestRate;
+
+	public BrazilInterestService(Double interestRate) {
+		this.interestRate = interestRate;
+	}
+
+	@Override
+	public double getInterestRate() {
+		return interestRate;
+	}
+
+	public void setInterestRate(Double interestRate) {
+		this.interestRate = interestRate;
+	}
+	
+	public double payment(double amount, int months) {
+		if (months < 1) {
+			throw new InvalidParameterException("Months must be greater than zero");
+		}
+		return amount * Math.pow(1 + interestRate / 100, months);
+	}
+}
